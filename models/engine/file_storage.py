b22b9ad0f19011780032d3798ab8b57f2b6fcab5
globalsmile <mohammedbadmus823@gmail.com>
2023-12-16 14:21:02 +0100

    update: file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
     """serializes instances to a JSON file & deserializes back to instances"""
 
     # string - path to the JSON file
-    __file_path = "file.json"
+    __file_path = "data.json"
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
